f83827d730e6db22acd68fc3078ab824be90ba55
Everardo Maya <maya.everardo@gmail.com>
2021-06-08 21:38:24 -0700

    added endpoint to serve job postings by industry

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template, jsonify, make_response
+from flask import Flask, render_template, jsonify, make_response, Response
 import pandas as pd
 from sqlalchemy import create_engine, func, MetaData, Table
 from sqlalchemy.orm import Session
 import os
 from config import get_config
+import numpy as np
 from pprint import pprint
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_agg import FigureCanvasAgg
+import random
+import io
 
 app = Flask(__name__)
 
@@ -68,6 +73,15 @@ def load_data():
         'Revenue': 'revenue'
     }).copy()
 
+    # replace -1 values with NaN using np
+    jobs_df = jobs_df.replace('-1', np.nan)
+
+    # drop all NaN values
+    jobs_df = jobs_df.dropna(axis=0, how='any')
+
+    # drop all locations not specified with city, that only list United States
+    jobs_df = jobs_df[jobs_df['location'] != 'United States']
+
     # get sqlalchemy engine
     engine = get_db_engine()
 
@@ -107,12 +121,142 @@ def job_openings_totals_by_position():
     res = make_response(jsonify(results), 200)
     return res
 
+@app.route("/api/data-science/job-openings/totals_by_location")
+def job_openings_totals_by_location():
+    # get sqlalchemy engine
+    engine = get_db_engine()
+
+    # get reflected Job table
+    Jobs = get_jobs_tbl(engine)
+
+    with engine.connect() as con:
+        session = Session(con)
+        job_count_totals = func.count(Jobs.columns.job_title)
+        results = session.query(Jobs.columns.location, job_count_totals) \
+            .group_by(Jobs.columns.location) \
+            .order_by(job_count_totals.desc()) \
+            .all()
+
+    res = make_response(jsonify(results), 200)
+    return res
+
+@app.route("/api/data-science/job-openings/totals_by_industry.png")
+def job_openings_totals_by_industry_png():
+    # get sqlalchemy engine
+    engine = get_db_engine()
+
+    # get reflected Job table
+    Jobs = get_jobs_tbl(engine)
+
+    industry_df = None
+
+    # load dataframe using sqlalchemy
+    with engine.connect() as con:
+        session = Session(con)
+        job_count_totals = func.count(Jobs.columns.job_title)
+        statement = session.query(Jobs.columns.industry, job_count_totals) \
+            .group_by(Jobs.columns.industry) \
+            .order_by(job_count_totals.desc()) \
+            .statement
+        industry_df = pd.read_sql(statement, session.bind)
+
+    industry_df = industry_df.rename(columns={'count_1': 'postings'})
+
+    # Create circle bar for job posting count for each industry
+    # https://www.python-graph-gallery.com/circular-barplot-basic
+
+    fig = Figure(figsize=(12,10))
+
+    ax = fig.add_subplot(111, polar=True)
+    ax.axis('off')
+
+    # Constants = parameters controling the plot layout:
+    upperLimit = 66
+    lowerLimit = 8
+    labelPadding = 4
+
+    # Compute max and min in the dataset
+    max = industry_df.postings.max()
+
+    # Let's compute heights: they are a conversion of each item value in those new coordinates
+    # In our example, 0 in the dataset will be converted to the lowerLimit (10)
+    # The maximum will be converted to the upperLimit (100)
+    slope = (max - lowerLimit) / max
+    heights = slope * industry_df.postings + lowerLimit
+
+    # Compute the width of each bar. In total we have 2*Pi = 360°
+    width = 2 * np.pi / len(industry_df.index)
+
+    # Compute the angle each bar is centered on:
+    indexes = list(range(1, len(industry_df.index)+1))
+    angles = [element * width for element in indexes][::-1]
+    angles
+
+    # Draw bars
+    bars = ax.bar(
+        x=angles,
+        height=heights,
+        width=width,
+        bottom=lowerLimit,
+        linewidth=2,
+        edgecolor="white",
+        color="#33FF82",
+    )
+
+    # Add labels
+    for bar, angle, height, label in zip(bars,angles, heights, industry_df["industry"]):
+
+        # Labels are rotated. Rotation must be specified in degrees :(
+        rotation = np.rad2deg(angle)
+
+        # Flip some labels upside down
+        alignment = ""
+        if angle >= np.pi/2 and angle < 3*np.pi/2:
+            alignment = "right"
+            rotation = rotation + 180
+        else:
+            alignment = "left"
+
+        # Finally add the labels
+        ax.text(
+            x=angle,
+            y=lowerLimit + bar.get_height() + labelPadding,
+            s=label,
+            ha=alignment,
+            va='center',
+            rotation=rotation,
+            rotation_mode="anchor")
+
+    # capture image output
+    output = io.BytesIO()
+
+    canvas = FigureCanvasAgg(fig)
+    canvas.print_png(output)
+    return Response(output.getvalue(), mimetype='image/png')
+
+@app.route("/api/data-science/job-openings/average_salary.png")
+def job_openings_average_salary_png():
+    # get sqlalchemy engine
+    engine = get_db_engine()
+
+    # get reflected Job table
+    Jobs = get_jobs_tbl(engine)
+
+    jobs_df = None
+
+    # load dataframe using sqlalchemy
+    with engine.connect() as con:
+        session = Session(con)
+        statement = session.query(Jobs).statement
+        jobs_df = pd.read_sql(statement, session.bind)
+
+    #TODO: implement salary graph
+    return make_response(jobs_df.to_html(), 200)
 
 @app.route("/api/data-science/highest-demand")
 def highest_demand():
     res = make_response(jsonify({"highest-demand": 30}), 200)
     return res
-
 
 @app.route("/api/data-science/average-salary")
 def average_salary():
--- static/js/app.js
+++ static/js/app.js
@@ -5,11 +5,25 @@ d3.json("/api/data-science/load-data").then(data => {
     d3.json("/api/data-science/job-openings/totals_by_position").then(data => {
         console.log(data);
         d3.select('#jobs_by_position')
-            .selectAll('ul')
+            .append('table')
+            .selectAll('tbody')
             .data(data)
             .enter()
-            .append('li')
-            .text(entry => entry[0])
+            .append('tr')
+            .html(entry => entry.map(col => {
+                return `<td>${col}<td>`;
+            }).join(''))
+    });
+
+    d3.select('#totals_by_industry')
+        .attr("src", "/api/data-science/job-openings/totals_by_industry.png")
+        .style("width", "25em")
+        .style("height", "20em");
+
+    d3.xml("/api/data-science/job-openings/average_salary.png").then(console.log);
+
+    d3.json("/api/data-science/job-openings/totals_by_location").then(data => {
+        console.log('totals_by_location: ', data);
     });
 
     d3.json("/api/data-science/highest-demand").then(data => {
